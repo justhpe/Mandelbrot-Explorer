695850ab002d4750869f6f327f04cee076459479
justhpe <146362139+justhpe@users.noreply.github.com>
2026-06-02 21:09:02 +0200

    create app using imgui

--- Mandelbrot-Explorer/Mandelbrot-Explorer.cpp
+++ Mandelbrot-Explorer/Mandelbrot-Explorer.cpp
@@ -1,8 +1,84 @@
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <iostream>
+
+#include <glfw3.h>
 
 int main()
 {
 
-    std::cout << "Hello world!";
+    // Config
+    short width = 1280;
+    short height = 720;
+
+    // GLFW Init
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // OpenGL 3.3 Core Profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Creating a system window
+    GLFWwindow* window = glfwCreateWindow(width, height, "Mandelbrot Explorer", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // V-Sync
+
+    // Dear ImGui Init
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Connecting ImGui to GLFW and OpenGL
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        // Event handling
+        glfwPollEvents();
+
+        // Start new ImGui
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            ImGui::Begin("Settings");
+
+            ImGui::Text("Mandelbrot Explorer");
+
+            ImGui::End();
+        }
+        
+        // Rendering ImGui
+        ImGui::Render();
+
+        // Screen cleaning (OpenGL)
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Drawing the interface on the screen
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        // Displaying a frame on the screen
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    
 
 }
